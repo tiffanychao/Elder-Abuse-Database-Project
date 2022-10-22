2c051038df9fd22b559e485db8835aaa354258c5
Justin <justingrant99@me.com>
2022-10-22 11:34:24 -0700

    fix: changed routes for homepage and client info

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+
+@app.route('/client_information')
+def clinet_information():
     return render_template('clientInformation.html', sampleInfo = "sampleInfo", exampleCheckbox = "1")
 
-@app.route('/test')
-def test():
-    return render_template('framework.html')
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
