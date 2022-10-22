4d0f3d6e58b450b052e8e9fbcdd4c4fcc2d6d112
Justin <justingrant99@me.com>
2022-10-22 11:18:40 -0700

    feat: finished the client information page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('clientInformation.html', sampleInfo = "sampleInfo", exampleCheckbox = "1")
+
+@app.route('/test')
+def test():
+    return render_template('framework.html')
+
